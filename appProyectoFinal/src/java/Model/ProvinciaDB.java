0ee5315c39dcda593b689ea557e65c4456b4e523
luubs <luubs@10.10.47.92>
2019-07-29 16:42:45 -0600

    CambioProvincia

--- appProyectoFinal/src/java/Model/ProvinciaDB.java
+++ appProyectoFinal/src/java/Model/ProvinciaDB.java
@@ -1,14 +1,61 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Model;
+
+import DAO.AccesoDatos;
+import DAO.SNMPExceptions;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedList;
 
 /**
  *
  * @author MrHaksh
  */
 public class ProvinciaDB {
-    
+
+    public ProvinciaDB() {
+    }
+
+    //metodo que se trae toda la lista de provincias
+    public LinkedList<Provincia> moTodo() throws SNMPExceptions, SQLException {
+
+        String select = "";
+
+        LinkedList<Provincia> listaPro = new LinkedList<Provincia>();
+
+        try {
+            //Se intancia la clase de acceso a datos
+            AccesoDatos accesoDatos = new AccesoDatos();
+
+            //Se crea la sentencia de Busqueda
+            select
+                    = "Select COD_PROVINCIA,DSC_CORTA_PROVINCIA,DSC_PROVINCIA,LOG_ACTIVO FROM PROVINCIAS";
+            //se ejecuta la sentencia sql
+            ResultSet rsPA = accesoDatos.ejecutaSQLRetornaRS(select);
+            //se llama el array con los proyectos
+            while (rsPA.next()) {
+
+                int codProvincia = rsPA.getInt("COD_PROVINCIA");
+                String desCorta = rsPA.getString("DSC_CORTA_PROVINCIA");
+                String descripcion = rsPA.getString("DSC_PROVINCIA");
+                int logActivo = rsPA.getInt("LOG_ACTIVO");
+
+                //se construye el objeto.
+                Provincia prov = new Provincia(codProvincia,
+                        desCorta, descripcion, logActivo);
+
+                listaPro.add(prov);
+            }
+            rsPA.close();//se cierra el ResultSeat.
+
+        } catch (SQLException e) {
+            throw new SNMPExceptions(SNMPExceptions.SQL_EXCEPTION,
+                    e.getMessage(), e.getErrorCode());
+        } catch (Exception e) {
+            throw new SNMPExceptions(SNMPExceptions.SQL_EXCEPTION, e.getMessage());
+        } finally {
+
+        }
+        return listaPro;
+    }
+
 }
