b580cf7cfac52bb72810c467cf4a97eb13e1fba8
MrHaksh <MrHaksh@MrHaksh>
2019-08-05 13:01:34 -0600

    Se agreho Metodo obtener todo de TipoIdentificaicon y Disciplina

--- appProyectoFinal/src/java/Model/DisciplinaDB.java
+++ appProyectoFinal/src/java/Model/DisciplinaDB.java
@@ -5,10 +5,62 @@
  */
 package Model;
 
+import DAO.AccesoDatos;
+import DAO.SNMPExceptions;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedList;
+
 /**
  *
  * @author MrHaksh
  */
 public class DisciplinaDB {
     
+    public LinkedList<Disciplina> moTodo() throws SNMPExceptions, SQLException {
+
+        String select = "";
+
+        LinkedList<Disciplina> listaDisciplina = new LinkedList<Disciplina>();
+
+        try {
+            //Se intancia la clase de acceso a datos
+            AccesoDatos accesoDatos = new AccesoDatos();
+
+            /*
+            Disciplina(
+            idDisciplina int IDENTITY(1,1) not null,
+            descripcion varchar(100),
+            LOG_ACTIVO bit
+            );
+             */
+            //Se crea la sentencia de Busqueda
+            select
+                    = "Select idDisciplina, descripcion, LOG_ACTIVO FROM Disciplina";
+            //se ejecuta la sentencia sql
+            ResultSet rsPA = accesoDatos.ejecutaSQLRetornaRS(select);
+            //se llama el array con los proyectos
+            while (rsPA.next()) {
+
+                int idDisciplina = rsPA.getInt("idDisciplina");
+                String descripcion = rsPA.getString("descripcion");
+                int logActivo = rsPA.getInt("LOG_ACTIVO");
+
+                //se construye el objeto.
+                Disciplina dis = new Disciplina(idDisciplina, descripcion, logActivo);
+
+                listaDisciplina.add(dis);
+            }
+            rsPA.close();//se cierra el ResultSeat.
+
+        } catch (SQLException e) {
+            throw new SNMPExceptions(SNMPExceptions.SQL_EXCEPTION,
+                    e.getMessage(), e.getErrorCode());
+        } catch (Exception e) {
+            throw new SNMPExceptions(SNMPExceptions.SQL_EXCEPTION, e.getMessage());
+        } finally {
+
+        }
+        return listaDisciplina;
+    }
 }
--- appProyectoFinal/src/java/Model/TipoIdentificacionDB.java
+++ appProyectoFinal/src/java/Model/TipoIdentificacionDB.java
@@ -5,10 +5,66 @@
  */
 package Model;
 
+import DAO.AccesoDatos;
+import DAO.SNMPExceptions;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedList;
+
 /**
  *
  * @author MrHaksh
  */
 public class TipoIdentificacionDB {
-    
+
+    public TipoIdentificacionDB() {
+    }
+
+    //metodo que se trae toda la lista de tipo identificacion
+    public LinkedList<TipoIdentificacion> moTodo() throws SNMPExceptions, SQLException {
+
+        String select = "";
+
+        LinkedList<TipoIdentificacion> listaTipoIdentificacion = new LinkedList<TipoIdentificacion>();
+
+        try {
+            //Se intancia la clase de acceso a datos
+            AccesoDatos accesoDatos = new AccesoDatos();
+
+            /*
+            TipoIdentificacion(
+            idTipoIdentificacion int IDENTITY(1,1) not null,
+            descripcion varchar(150),
+            LOG_ACTIVO bit
+            );
+             */
+            //Se crea la sentencia de Busqueda
+            select
+                    = "Select idTipoIdentificacion, descripcion, LOG_ACTIVO FROM TipoIdentificacion";
+            //se ejecuta la sentencia sql
+            ResultSet rsPA = accesoDatos.ejecutaSQLRetornaRS(select);
+            //se llama el array con los proyectos
+            while (rsPA.next()) {
+
+                int idtipoiden = rsPA.getInt("idTipoIdentificacion");
+                String descripcion = rsPA.getString("descripcion");
+                int logActivo = rsPA.getInt("LOG_ACTIVO");
+
+                //se construye el objeto.
+                TipoIdentificacion ti = new TipoIdentificacion(idtipoiden, descripcion, logActivo);
+
+                listaTipoIdentificacion.add(ti);
+            }
+            rsPA.close();//se cierra el ResultSeat.
+
+        } catch (SQLException e) {
+            throw new SNMPExceptions(SNMPExceptions.SQL_EXCEPTION,
+                    e.getMessage(), e.getErrorCode());
+        } catch (Exception e) {
+            throw new SNMPExceptions(SNMPExceptions.SQL_EXCEPTION, e.getMessage());
+        } finally {
+
+        }
+        return listaTipoIdentificacion;
+    }
 }
